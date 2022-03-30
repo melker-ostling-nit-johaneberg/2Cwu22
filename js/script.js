1ecb46a66dfe0281de94bdf92901510c6db9f3a8
melker-ostling-nit-johaneberg <melker.ostling@elev.ga.ntig.se>
2022-03-30 23:12:04 +0200

    Hemma onsdag v:13

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 function HamburgerMeny(){
   let nav_mobil = document.getElementById("nav_mobil");
   let top = document.getElementById("top")
+  let top_2 = document.getElementById("top_2")
   if (nav_mobil.style.display === "block") {
     nav_mobil.style.display = "none";
     top.style.marginTop = "0vh"
+    top_2.style.marginTop = "0vh"
   } else {
     nav_mobil.style.display = "block";
-    top.style.marginTop = "7.5vh"
+    top.style.marginTop = "5vh"
+    top_2.style.marginTop = "5vh"
 
   }
 }
