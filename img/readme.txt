89c0c08468cda5896a574ae8ad3de7dc3c04a077
melker-ostling-nit-johaneberg <melker.ostling@elev.ga.ntig.se>
2022-04-04 00:23:59 +0200

    Förhoppningsvis sista

--- img/readme.txt
+++
@@ -1,1 +0,0 @@
-Förminska bilder
--- js/script.js
+++ js/script.js
@@ -3,15 +3,16 @@ function HamburgerMeny(){
   let nav_mobil = document.getElementById("nav_mobil");
   let top = document.getElementById("top");
   let top_2 = document.getElementById("top_2");
-  console.log(nav_bars)
   if (nav_mobil.style.display === "block") {
     nav_mobil.style.display = "none"
     top.style.marginTop = "0vh"
-    top_2.style.marginTop = "0vh"
+    top_2.style.marginTop = "0vh"  
+
   } else {
     nav_mobil.style.display = "block"
     top.style.marginTop = "6vh"
     top_2.style.marginTop = "6vh"
+
   }
 }
 
